e61bc6d8fde2b630796284173f5ad6c134f56381
aridiz <ariannadiaz@mail.com>
2023-07-09 11:46:28 +0200

    testing a timer

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react"; //used most
 import Header from "./header";
 
 const App = () => {
@@ -6,6 +6,20 @@ const App = () => {
     //state variable: reactive variable that changes
     //array: current value, function update 
     const [counter, setCounter] = useState(0);
+
+    useEffect( () => {
+        //anything external to React can be done here
+        //side effects to the component: calling data API to fetch
+        const intervalId = setInterval( () => {
+            setCounter(counter +1);
+        }, 1000)
+
+        return () => {
+            //any clean up from the dom
+            //stop the timer
+            clearInterval(intervalId);
+        }
+    })
     return (
     <div className="container">
         <Header message="Naming Contests" />
