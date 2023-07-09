d6ad8f86f103f42cfc04eee21165dd55b136bb7a
aridiz <ariannadiaz@mail.com>
2023-07-09 18:15:17 +0200

    api-client added

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -1,15 +1,25 @@
 import ContestPreview from "./contest-preview";
+import { useEffect, useState } from "react"; 
+import { fetchContests } from "../api-client";
 
-const ContestList = ({ contests}) => {
+const ContestList = ({ initialContests }) => {
+    const [contests, setContests] = useState(initialContests);
+
+    useEffect(() => {
+    fetchContests().then((data) => { //call the function and then.. 
+        setContests(data.contests); 
+    }); 
+}, []); //after the first render stops re-rendering
+
     return (
-<div className="contest-list">
-            {contests.map((contest) => {
-               return (
-               <ContestPreview key={contest.id} contest={contest} />
-               );
-            })}
-        </div>
-    );
+        <div className="contest-list">
+                    {contests.map((contest) => {
+                    return (
+                    <ContestPreview key={contest.id} contest={contest} />
+                    );
+                    })}
+                </div>
+            );
 };
 
 export default ContestList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 //this file is going to contain some jsx
 //react-dom
 import {createRoot} from "react-dom/client"
-import axios from "axios";
-
-import { API_SERVER_URL } from "./public-config";
 
 import App from "./components/app";
 
@@ -11,10 +8,5 @@ import App from "./components/app";
 const container = document.getElementById("app");
 const root = createRoot(container);
 
-//JS Promis
-axios.get(`${API_SERVER_URL}/contests`).then((resp) => {
-    console.log(resp);
-    root.render(<App initialData={ {contests: resp.data.contests} } />
-    ); //react component to be rendered
-});
-
+root.render(<App initialData={ {contests: []} } />
+); //react component to be rendered
