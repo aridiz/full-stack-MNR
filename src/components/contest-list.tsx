4cc9ea06cc347056d7c356abde5d0257ff4670b7
aridiz <ariannadiaz@mail.com>
2023-07-09 17:55:42 +0200

    key error removed

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -1,10 +1,12 @@
 import ContestPreview from "./contest-preview";
 
-const ContestList = ({contests}) => {
+const ContestList = ({ contests}) => {
     return (
 <div className="contest-list">
             {contests.map((contest) => {
-               return <ContestPreview contest={contest} />;
+               return (
+               <ContestPreview key={contest.id} contest={contest} />
+               );
             })}
         </div>
     );
