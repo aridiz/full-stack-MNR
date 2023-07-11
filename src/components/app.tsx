0434d43f555c8c021ddad951a090083322a513dd
aridiz <ariannadiaz@mail.com>
2023-07-11 16:25:58 +0200

    dynamic URL

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,6 @@
 // import { useState } from "react"; //used most
 import ContestList from "./contest-list";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Contest from "./contest";
 
 const App = ( {initialData} ) => {
@@ -8,10 +8,22 @@ const App = ( {initialData} ) => {
     const [page, setPage] = useState<"contestList" | "contest">("contestList",);
     const [currentContestId, setCurrentContestId] = useState<string | undefined>(); //undefined as default
     
+    //manage path when clicking on back and forward on the browser
+    useEffect(() => {
+        window.onpopstate = (event) => {
+            const newPage = event.state?.contestId 
+            ? "contest" 
+            : "contestList";
+            setPage(newPage);
+            setCurrentContestId(event.state?.contestId);
+        };
+    }, []);
+
     const navigateToContest = (contestId) => {
+        window.history.pushState({contestId}, "", `/contest/${contestId}`); //dynamic URL
         setPage("contest");
         setCurrentContestId(contestId);
-    }
+    };
 
     const pageContent = () => {
         switch (page) {
